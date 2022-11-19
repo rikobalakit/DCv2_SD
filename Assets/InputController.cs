477da8f11520dc20ef2cb0910ef8f3033fa7f6c0
ricobalakit <rico@balak.it>
2022-11-18 23:57:32 -0600

    joystick fix

--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -9,18 +9,32 @@ public class InputController : MonoBehaviour
 
     [SerializeField]
     private TextMeshPro _rxText;
+
+    [SerializeField]
+    private RectTransform _dotL;
+
+    [SerializeField]
+    private RectTransform _dotR;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        float lx = Input.GetAxis("LX");
+        float ly = Input.GetAxis("LY");
         float rx = Input.GetAxis("RX");
-        
-        Debug.Log($"rx: {rx}");
+        float ry = Input.GetAxis("RY");
+
+        Debug.Log($"lx: {lx}, ly: {ly}, rx: {rx}, ry: {ry}, ");
+        _dotL.anchoredPosition = new Vector2(lx * 40, ly * -40);
+        _dotR.anchoredPosition = new Vector2(rx * 40, ry * -40);
+
     }
+
 }
