648c11e3d5e1beaf5dc3a6aa3396dc69860fed7d
ricobalakit <rico@balak.it>
2022-11-19 04:25:05 -0600

    get rid of all but one characteristic 2

--- Assets/BluetoothConsole.cs
+++ Assets/BluetoothConsole.cs
@@ -164,9 +164,11 @@ public class BluetoothConsole : MonoBehaviour
         }
 
         var taskS = JoystickTask(ctrlCharacteristic);
+        /*
         var taskX = SensorXTask(orientationXCharacteristic);
         var taskY = SensorYTask(orientationYCharacteristic);
         var taskZ = SensorZTask(orientationZCharacteristic);
+        */
 
         await Task.WhenAll(taskS);
 
