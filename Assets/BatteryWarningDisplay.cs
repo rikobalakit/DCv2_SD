df1fe0229542f8bf8587bfeaefba56632be14489
ricobalakit <rico@balak.it>
2023-03-18 15:06:44 -0500

    battery warnings less aggro, added critical and dead distinction

--- Assets/BatteryWarningDisplay.cs
+++ Assets/BatteryWarningDisplay.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,9 +11,10 @@ public class BatteryWarningDisplay : MonoBehaviour
     {
         USB = 0,
         Dead = 1,
-        Low = 2,
-        High = 3,
-        Full = 4
+        Critical= 2,
+        Low = 3,
+        High = 4,
+        Full = 5
         
 
     }
@@ -24,6 +26,9 @@ public class BatteryWarningDisplay : MonoBehaviour
     private List<GameObject> _lowBatteryWarningObjects;
     
     [SerializeField]
+    private List<GameObject> _criticalBatteryWarningObjects;
+    
+    [SerializeField]
     private List<GameObject> _deadBatteryWarningObjects;
     
     [SerializeField]
@@ -31,41 +36,67 @@ public class BatteryWarningDisplay : MonoBehaviour
 
     [SerializeField]
     private AudioSource _lowBatteryAudio;
+    
+    [SerializeField]
+    private AudioSource _criticalBatteryAudio;
 
     [SerializeField]
     private AudioSource _deadBatteryAudio;
     
     private BatteryStatus _currentBatteryStatus = BatteryStatus.Dead;
 
-    private const float MINIMUM_VOLTAGE_BATTERY_FULL = 15.600f;
+    private const float MINIMUM_VOLTAGE_BATTERY_FULL = 16.600f;
+    private const float MINIMUM_VOLTAGE_BATTERY_HIGH = 15.200f;
     private const float MINIMUM_VOLTAGE_BATTERY_LOW = 14.000f;
     private const float MINIMUM_VOLTAGE_BATTERY_DEAD = 13.200f;
     private const float MINIMUM_VOLTAGE_USING_USB = 6.000f;
 
     private float _audioLastPlayedTime = float.MinValue;
-    private const float AUDIO_COOLDOWN_TIME_SECONDS = 2f;
+    private float _audioCooldownTime = 2f;
+
+    private float _lastTimeBatteryWasStable = float.MinValue;
+    private const float BATTERY_STABILITY_PERIOD_SECONDS = 0.5f;
     
     void Update()
     {
+        BatteryStatus newBatteryStatus = BatteryStatus.USB;
+        
         if (_telemetryValues.BatteryVoltage >= MINIMUM_VOLTAGE_BATTERY_FULL)
         {
-            _currentBatteryStatus = BatteryStatus.Full;
+            newBatteryStatus = BatteryStatus.Full;
         }
-        else if (_telemetryValues.BatteryVoltage >= MINIMUM_VOLTAGE_BATTERY_LOW &&  _telemetryValues.BatteryVoltage < MINIMUM_VOLTAGE_BATTERY_FULL )
+        else if (_telemetryValues.BatteryVoltage >= MINIMUM_VOLTAGE_BATTERY_HIGH &&  _telemetryValues.BatteryVoltage < MINIMUM_VOLTAGE_BATTERY_FULL )
         {
-            _currentBatteryStatus = BatteryStatus.High;
+            newBatteryStatus = BatteryStatus.High;
+        }
+        else if (_telemetryValues.BatteryVoltage >= MINIMUM_VOLTAGE_BATTERY_LOW &&  _telemetryValues.BatteryVoltage < MINIMUM_VOLTAGE_BATTERY_HIGH )
+        {
+            newBatteryStatus = BatteryStatus.Low;
         }
         else if (_telemetryValues.BatteryVoltage >= MINIMUM_VOLTAGE_BATTERY_DEAD &&  _telemetryValues.BatteryVoltage < MINIMUM_VOLTAGE_BATTERY_LOW )
         {
-            _currentBatteryStatus = BatteryStatus.Low;
+            newBatteryStatus = BatteryStatus.Critical;
         }
         else if (_telemetryValues.BatteryVoltage < MINIMUM_VOLTAGE_BATTERY_DEAD && _telemetryValues.BatteryVoltage > MINIMUM_VOLTAGE_USING_USB)
         {
-            _currentBatteryStatus = BatteryStatus.Dead;
+            newBatteryStatus = BatteryStatus.Dead;
         }
         else if (_telemetryValues.BatteryVoltage <= MINIMUM_VOLTAGE_USING_USB)
         {
-            _currentBatteryStatus = BatteryStatus.USB;
+            newBatteryStatus = BatteryStatus.USB;
+        }
+
+        if (_currentBatteryStatus != newBatteryStatus)
+        {
+            if (Time.time - _lastTimeBatteryWasStable > BATTERY_STABILITY_PERIOD_SECONDS)
+            {
+                _currentBatteryStatus = newBatteryStatus;
+                _lastTimeBatteryWasStable = Time.time;
+            }
+        }
+        else
+        {
+            _lastTimeBatteryWasStable = Time.time;
         }
 
         UpdateDisplay(_currentBatteryStatus);
@@ -77,6 +108,11 @@ public class BatteryWarningDisplay : MonoBehaviour
         foreach (var lowBatteryObject in _lowBatteryWarningObjects)
         {
             lowBatteryObject.SetActive(newBatteryStatus == BatteryStatus.Low);
+        }
+        
+        foreach (var criticalBatteryObject in _criticalBatteryWarningObjects)
+        {
+            criticalBatteryObject.SetActive(newBatteryStatus == BatteryStatus.Critical);
         }
             
         foreach (var deadBatteryObject in _deadBatteryWarningObjects)
@@ -92,7 +128,32 @@ public class BatteryWarningDisplay : MonoBehaviour
 
     private void UpdateAudio(BatteryStatus newBatteryStatus)
     {
-        if (_deadBatteryAudio.isPlaying || _lowBatteryAudio.isPlaying || Time.time - _audioLastPlayedTime < AUDIO_COOLDOWN_TIME_SECONDS)
+        switch (newBatteryStatus)
+        {
+
+            case BatteryStatus.USB:
+                _audioCooldownTime = 10f;
+                break;
+            case BatteryStatus.Dead:
+                _audioCooldownTime = 2f;
+                break;
+            case BatteryStatus.Critical:
+                _audioCooldownTime = 2f;
+                break;
+            case BatteryStatus.Low:
+                _audioCooldownTime = 10f;
+                break;
+            case BatteryStatus.High:
+                _audioCooldownTime = 10f;
+                break;
+            case BatteryStatus.Full:
+                _audioCooldownTime = 10f;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newBatteryStatus), newBatteryStatus, null);
+        }
+        
+        if (_deadBatteryAudio.isPlaying || _lowBatteryAudio.isPlaying || Time.time - _audioLastPlayedTime < _audioCooldownTime)
         {
             return;
         }
@@ -101,6 +162,13 @@ public class BatteryWarningDisplay : MonoBehaviour
         {
             Debug.Log("Playing low battery audio");
             _lowBatteryAudio.Play();
+            _audioLastPlayedTime = Time.time;
+        }
+        
+        if (newBatteryStatus == BatteryStatus.Critical)
+        {
+            Debug.Log("Playing critical battery audio");
+            _criticalBatteryAudio.Play();
             _audioLastPlayedTime = Time.time;
         }
         
